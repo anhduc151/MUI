17b863df2d9be9166189952c492d046821134a30
anhduc151 <leanhduc1510@gmail.com>
2024-01-11 11:16:00 +0700

    mui

---
+++ src/pages/Home.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Button from "@mui/material/Button";
+// import { AccessAlarm, ThreeDRotation } from "@mui/icons-material";
+// import Radio from "@mui/material/Radio";
+// import RadioGroup from "@mui/material/RadioGroup";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import FormControl from "@mui/material/FormControl";
+// import FormLabel from "@mui/material/FormLabel";
+// add styled custom hover element
+import { styled } from "@mui/material/styles";
+
+const Home = () => {
+//     const [value, setValue] = React.useState("female");
+
+//   const handleChange = (event) => {
+//     setValue(event.target.value);
+//   };
+
+  const Div = styled("div")({
+    padding: "20px 20%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+
+    "&:hover": {
+      backgroundColor: "#e0e0e0",
+    },
+  });
+
+  const StyleButton = styled(Button)({
+    "&:hover": {
+      backgroundColor: "#000",
+      color: "white",
+    },
+  });
+  return (
+    <div>
+      <Div>
+        {/* <StyleButton
+          variant="contained"
+          sx={{ marginTop: "5px", width: "200px", background: "blue" }}
+        >
+          Add
+        </StyleButton> */}
+        {/* <AccessAlarm sx={{ fontSize: "100px", color: "pink" }} /> */}
+        {/* <ThreeDRotation /> */}
+
+        {/* <FormControl>
+          <FormLabel id="demo-controlled-radio-buttons-group">Gender</FormLabel>
+          <RadioGroup
+            name="controlled-radio-buttons-group"
+            value={value}
+            onChange={handleChange}
+          >
+            <FormControlLabel
+              value="female"
+              control={<Radio sx={{ fontSize: "30px", color: "red" }} />}
+              label="Female"
+            />
+            <FormControlLabel value="male" control={<Radio />} label="Male" />
+          </RadioGroup>
+        </FormControl> */}
+      </Div>
+    </div>
+  );
+};
+
+export default Home;
